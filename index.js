5f1c9b38e7716ece3988615a8aef42a3c75e63f1
Tyler <tyler.rodgers74@gmail.com>
2022-09-26 17:17:38 -0400

    added menu select

--- index.js
+++ index.js
@@ -26,4 +26,63 @@ connected = () => {
   console.log("*        EMPLOYEE MANAGER         *");
   console.log("*                                 *");
   console.log("***********************************");
+  menuPrompt();
+};
+
+// "main menu" prompt
+const menuPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "menuSelect",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Update Employee Role",
+          "View All Roles",
+          "Add Role",
+          "View All Departments",
+          "Add Department",
+          "Quit",
+        ],
+      },
+    ])
+    .then((answer) => {
+      const { menuSelect } = answer;
+
+      if (menuSelect === "View All Employees") {
+        console.log("You chose view all employees.");
+      }
+
+      if (menuSelect === "Add Employee") {
+        console.log("You chose add employee.");
+      }
+
+      if (menuSelect === "Update Employee Role") {
+        console.log("You chose update employee role.");
+      }
+
+      if (menuSelect === "View All Roles") {
+        console.log("You chose view all roles");
+      }
+
+      if (menuSelect === "Add Role") {
+        console.log("You chose add role");
+      }
+
+      if (menuSelect === "View All Departments") {
+        console.log("You chose view all departments");
+      }
+
+      if (menuSelect === "Add Department") {
+        console.log("You chose add department");
+      }
+
+      if (menuSelect === "Quit") {
+        console.log("You chose to quit. Goodbye!");
+        connection.end();
+      }
+    });
 };
